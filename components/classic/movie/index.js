719f7819329e99dbb275d79e83d65835f3888c55
Flanderz <zzytroyzhang@gmail.com>
2021-03-12 19:28:24 +0800

    second commit

--- components/classic/movie/index.js
+++ components/classic/movie/index.js
@@ -1,10 +1,11 @@
-// components/classic/movie/movie.js
+// components/classic/movie/index.js
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+      img:String,
+      content:String
   },
 
   /**
